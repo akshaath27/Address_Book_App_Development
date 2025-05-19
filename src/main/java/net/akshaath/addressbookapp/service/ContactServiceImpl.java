1eb23588686364fdf7e5b4a80af4b97a557b9f5e
Akshaath <nakshaath@gmail.com>
2025-05-19 15:47:23 +0530

    UC5_Full_Controller_with_CRUD_using_DTO_and_Service

---
+++ src/main/java/net/akshaath/addressbookapp/service/ContactServiceImpl.java
@@ -0,0 +1,51 @@
+package net.akshaath.addressbookapp.service;
+
+import net.akshaath.addressbookapp.dto.ContactDTO;
+import net.akshaath.addressbookapp.model.Contact;
+import net.akshaath.addressbookapp.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContactServiceImpl implements ContactService {
+
+    @Autowired
+    private ContactRepository repository;
+
+    @Override
+    public Contact addContact(ContactDTO dto) {
+        Contact contact = new Contact(dto.getid(),dto.getFullName(), dto.getEmail(), dto.getPhone(), dto.getCity());
+        return repository.save(contact);
+    }
+
+    @Override
+    public List<Contact> getAllContacts() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Contact getContactById(Long id) {
+        return repository.findById(id).orElseThrow(() ->
+                new RuntimeException("Contact not found with ID: " + id));
+    }
+
+    @Override
+    public Contact updateContact(Long id, ContactDTO dto) {
+        Contact contact = getContactById(id);
+        contact.setFullName(dto.getFullName());
+        contact.setEmail(dto.getEmail());
+        contact.setPhone(dto.getPhone());
+        contact.setCity(dto.getCity());
+        return repository.save(contact);
+    }
+
+    @Override
+    public void deleteContact(Long id) {
+        if (!repository.existsById(id)) {
+            throw new RuntimeException("Cannot delete. Contact not found with ID: " + id);
+        }
+        repository.deleteById(id);
+    }
+}
